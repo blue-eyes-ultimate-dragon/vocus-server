58297e68aa28def71307b719b4155e605522cf35
Nilushanan Kulasingham <nkulasingham@gmail.com>
2017-07-30 15:08:20 +1000

    ensure projects work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 require('dotenv').config();
 require('babel-register')();
 require('babel-polyfill');
-
 const log = require('fancy-log');
 const server = require('./server').default;
 
 const port = process.env.PORT || 5000;
+
+const xlsxj = require('xlsx-to-json');
+const path = require('path');
+
+const fs = require('fs');
+
+
+xlsxj(
+  {
+    input: path.resolve(__dirname, './workforce.xlsx'),
+    output: path.resolve(__dirname, './workforce.json'),
+  },
+  (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(result);
+    }
+  },
+);
+
 server.listen(port, () => log(`API server started on ${port}`));
